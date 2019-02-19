8254bdb296f6c284756cdaad6aafdacead98a19b
Felipe Jordão A.P. Mattosinho <felipe.japm@gmail.com>
2019-02-19 18:35:40 -0300

    Change assets S3 bucket

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -37,7 +37,7 @@ if (process.env.NODE_ENV === 'production') {
       region: 'us-east-1'
     },
     s3UploadOptions: {
-      Bucket: 'quero-assets/web-app'
+      Bucket: 'quero-cdn/assets'
     }
   }));
 }
