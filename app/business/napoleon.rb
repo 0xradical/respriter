d0f0cd01c3026e2e2e26b9190b374e802af20299
Felipe Jordão A.P. Mattosinho <felipe.japm@gmail.com>
2019-02-28 20:44:15 -0300

    Improve log readbility

--- app/business/napoleon.rb
+++ app/business/napoleon.rb
@@ -26,6 +26,8 @@ module Napoleon
     end
 
     def resources(global_sequence, &blk)
+      start, run = Time.now, SecureRandom.hex(6)
+      log(:info,"Pulling courses from Napoleon. Starting on global_sequence #{global_sequence}",["BEGIN","run.#{run}"])
       loop do
         resources = JSON.parse(http.get(URI % {global_sequence: global_sequence}).body)
         puts "Fetching global_sequence #{global_sequence}..."
@@ -35,12 +37,13 @@ module Napoleon
             check_version!(resource['content']['version'])
             blk.call(resource)
           rescue VersionNotSupported, ActiveRecord::RecordNotUnique, ActiveRecord::StatementInvalid => e
-            log('warn', "Fail to process resource #{resource['id']}: #{e.message}", ["gs #{resource['global_sequence']}"])
+            log(:warn, "Fail to process resource #{resource['id']}: #{e.message}", ["run.#{run}","gs #{resource['global_sequence']}"])
             next
           end
         end
         global_sequence = resources.last['global_sequence']
       end
+      log(:info,"Bye! End on global_sequence #{global_sequence}. Took #{Time.at((Time.now - start)).utc.strftime('%Hh %Mm %Ss')}",["END","run.#{run}"])
     end
 
     def log(level, msg, tags=nil)
