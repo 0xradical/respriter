2826db2e2ab64ba502dc412a70599173ec657924
Felipe Jordão A.P. Mattosinho <felipe.japm@gmail.com>
2019-03-01 14:01:07 -0300

    Improve Napoleon log messages

--- app/business/napoleon.rb
+++ app/business/napoleon.rb
@@ -14,9 +14,9 @@ module Napoleon
 
     class VersionNotSupported < RuntimeError; end;
 
-    attr_accessor :http
-
     URI = "https://napoleon-the-crawler.herokuapp.com/resources/updates/%{global_sequence}"
+
+    attr_accessor :http
 
     def initialize
       @http             = HTTP
@@ -26,24 +26,27 @@ module Napoleon
     end
 
     def resources(global_sequence, &blk)
-      start, run = Time.now, SecureRandom.hex(6)
-      log(:info,"Pulling courses from Napoleon. Starting on global_sequence #{global_sequence}",["BEGIN","run.#{run}"])
+      started_at, count, errors, run = Time.now, 0, 0, SecureRandom.hex(4)
+      log(:info,  "Pulling courses ...",["BEGIN","run.#{run}", "seq.#{global_sequence}".ansi(:blue)])
       loop do
         resources = JSON.parse(http.get(URI % {global_sequence: global_sequence}).body)
-        puts "Fetching global_sequence #{global_sequence}..."
         break if resources.empty?
         resources.each do |resource|
           begin
             check_version!(resource['content']['version'])
             blk.call(resource)
           rescue VersionNotSupported, ActiveRecord::RecordNotUnique, ActiveRecord::StatementInvalid => e
-            log(:warn, "Fail to process resource #{resource['id']}: #{e.message}", ["run.#{run}","gs #{resource['global_sequence']}"])
+            errors += 1
+            log(:warn,  "Fail to process resource: #{e.message}", ["run.#{run}", "seq.#{global_sequence}".ansi(:blue), "err.#{errors}".ansi(:red)])
+            log(:debug, "--inspect-- " + resource.to_s.ansi(:red), "err.#{errors}".ansi(:red))
             next
           end
+          count += 1
         end
         global_sequence = resources.last['global_sequence']
       end
-      log(:info,"Bye! End on global_sequence #{global_sequence}. Took #{Time.at((Time.now - start)).utc.strftime('%Hh %Mm %Ss')}",["END","run.#{run}"])
+      run_time = Time.at((Time.now - started_at)).utc.strftime('%Hh %Mm %Ss')
+      log(:info, "Bye! " + "Total: #{count} resources".ansi(:yellow) + " | " + (errors.zero? ? "Errors: #{errors.to_s}".ansi(:green) : "Errors: #{errors.to_s}".ansi(:red))+ " | " + "Took #{run_time}".ansi(:blue), ["END","run.#{run}", "seq.#{global_sequence}".ansi(:blue)])
     end
 
     def log(level, msg, tags=nil)
@@ -62,4 +65,20 @@ module Napoleon
 
   end
 
+end
+
+# Monkey Patch
+class String
+  def ansi(color)
+    case color
+      when :blue
+       return "\033[34m#{self}\033[0m"
+      when :red
+        return "\033[31;1m#{self}\033[0m"
+      when :yellow
+        return "\033[33m#{self}\033[0m"
+      when :green
+        return "\033[32m#{self}\033[0m"
+    end
+  end
 end
