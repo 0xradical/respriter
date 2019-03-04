8ce5cd57e3f3d21260a8c0f89b064e98331fda3f
Felipe Jordão A.P. Mattosinho <felipe.japm@gmail.com>
2019-03-03 23:26:47 -0300

    Track signups

--- app/controllers/user_accounts/registrations_controller.rb
+++ app/controllers/user_accounts/registrations_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class UserAccounts::RegistrationsController < Devise::RegistrationsController
-  # before_action :configure_sign_up_params, only: [:create]
+  before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
 
   # GET /resource/sign_up
@@ -42,11 +42,14 @@ class UserAccounts::RegistrationsController < Devise::RegistrationsController
   # end
 
   # protected
+  def sign_up_params
+    devise_parameter_sanitizer.sanitize(:sign_up).merge(tracking_data: session[:tracking_data])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_up_params
-  #   devise_parameter_sanitizer.permit(:sign_up, keys: [:attribute])
-  # end
+  def configure_sign_up_params
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:tracking_data])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_account_update_params
