2c902a32498e455319936b956fb9d76e9319375e
Felipe Jordão [codextremist] <felipe.japm@gmail.com>
2019-03-28 20:23:02 -0300

    Bugfix: typo on method name

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
 
-  http_basic_authentication_with(name: ENV['BASIC_AUTH_USER'], password: ENV['BASIC_AUTH_PASSWORD']) if ENV['BASIC_AUTH_REQUIRED']
+  http_basic_authenticate_with(name: ENV['BASIC_AUTH_USER'], password: ENV['BASIC_AUTH_PASSWORD']) if ENV['BASIC_AUTH_REQUIRED']
 
   protect_from_forgery with: :exception
   prepend_before_action :parse_browser_session
