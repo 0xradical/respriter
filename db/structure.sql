ca1b7448ae10c88a92e34395307210791990e1a0
Dalton <dalthon@dlt.local>
2019-05-13 14:17:34 +0200

    Update db/structure.sql

--- db/structure.sql
+++ db/structure.sql
@@ -442,7 +442,6 @@ CREATE TABLE public.tracked_actions (
     ext_id character varying,
     compound_ext_id character varying,
     ext_sku_id character varying,
-    ext_sku_name character varying,
     ext_product_name character varying
 );
 
@@ -457,19 +456,19 @@ CREATE VIEW public.earnings AS
     tracked_actions.earnings_amount,
     tracked_actions.ext_click_date,
     tracked_actions.ext_sku_id,
-    tracked_actions.ext_sku_name,
+    tracked_actions.ext_product_name,
     tracked_actions.ext_id,
     tracked_actions.source AS affiliate_network,
     providers.name AS provider_name,
     courses.name AS course_name,
     courses.url AS course_url,
+    (enrollments.tracking_data ->> 'country'::text) AS country,
     (enrollments.tracking_data ->> 'query_string'::text) AS qs,
     (enrollments.tracking_data ->> 'referer'::text) AS referer,
     (enrollments.tracking_data ->> 'utm_source'::text) AS utm_source,
     (enrollments.tracking_data ->> 'utm_campaign'::text) AS utm_campaign,
     (enrollments.tracking_data ->> 'utm_medium'::text) AS utm_medium,
     (enrollments.tracking_data ->> 'utm_term'::text) AS utm_term,
-    (enrollments.tracking_data ->> 'country'::text) AS country,
     tracked_actions.created_at
    FROM (((public.tracked_actions
      LEFT JOIN public.enrollments ON ((tracked_actions.enrollment_id = enrollments.id)))
