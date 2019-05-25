a13dc91ed87ddad8f240fb5357b1abcf03ca820a
Felipe Jordão [codextremist] <felipe.japm@gmail.com>
2019-05-25 20:10:45 -0300

    Small ui tweak

--- app/assets/vue/apps/user_dashboard/components/OauthAccount.vue
+++ app/assets/vue/apps/user_dashboard/components/OauthAccount.vue
@@ -2,7 +2,7 @@
   <div>
     <div class='mx-D(n)@<medium'>
       <a v-if='!connected' :href='authorizeUrl' :class='btnClass' style='max-width:200px'>  Connect with {{ provider }}</a>
-      <a v-else @click="$emit('destroyOauth', provider)" :class='btnClass' style='max-width:200px'>  Connected with {{ provider }}</a>
+      <a v-else @click="$emit('destroyOauth', provider)" :class='btnClass' style='max-width:250px'>  Connected with {{ provider }}</a>
     </div>
     <div class='mx-D(n)@>large'>
       <a v-if='!connected' :href='authorizeUrl' :class='btnClass'>  Connect with {{ provider }}</a>
