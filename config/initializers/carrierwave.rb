0d374f713166e98d339a1bf07c8bc2361e72c791
Dalton <dalthon@dlt.local>
2019-06-14 15:38:35 +0200

    Use ENV.fetch to explode error missing config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,15 @@
 CarrierWave.configure do |config|
   config.storage           = :aws
   config.aws_acl           = :public_read
-  config.aws_bucket        = ENV['AWS_S3_BUCKET_NAME']
-  config.asset_host        = ENV['AWS_S3_ASSET_HOST']
+  config.aws_bucket        = ENV.fetch 'AWS_S3_BUCKET_NAME'
+  config.asset_host        = ENV.fetch 'AWS_S3_ASSET_HOST'
   config.enable_processing = Rails.env.production?
 
   config.aws_credentials = {
-    endpoint:          ENV['AWS_S3_ENDPOINT'],
-    access_key_id:     ENV['AWS_ACCESS_KEY_ID'],
-    secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-    region:            ENV['AWS_S3_REGION'],
+    endpoint:          ENV.fetch('AWS_S3_ENDPOINT'),
+    access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
+    secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    region:            ENV.fetch('AWS_S3_REGION'),
     force_path_style:  !Rails.env.production?
   }
 
