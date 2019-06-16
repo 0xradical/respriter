839ba86a7ce5542aa4e1ca81638030a1e8e4457a
Felipe Jordão [codextremist] <felipe.japm@gmail.com>
2019-06-16 19:51:28 -0300

    Change method name

--- app/helpers/post_helper.rb
+++ app/helpers/post_helper.rb
@@ -1,6 +1,6 @@
 module PostHelper
 
-  def content_changed_after_period?(post, period: 1.month)
+  def content_changed_after?(post, period: 1.month)
     post.content_changed_at.to_date >= (post.published_at.to_date + period)
   end
 
