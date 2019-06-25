f86e0d6043fdecb8eee760d8f1d617d83402a33e
Felipe Jordão [codextremist] <felipe.japm@gmail.com>
2019-06-25 12:27:11 -0300

    Remove dead code

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,9 +2,6 @@ class HomeController < ApplicationController
 
   def index
     @count_by_category = Course.group('category').count
-    #@providers    = Provider.joins(:courses).group('providers.name').having("COUNT(*) > 35").order('count_all DESC').limit(15).count
-    @most_popular = Course.featured.locales([I18n.locale]).limit(8)
-    @free_courses = Course.free.locales([I18n.locale]).limit(8)
   end
 
 end
