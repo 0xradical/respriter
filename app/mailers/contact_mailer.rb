e4575c8af742708740aaa5d8ffb7d83339ee4cc2
Thiago Brandão <thiagobrandam@gmail.com>
2019-08-09 15:40:09 -0300

    Add to field for contact mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,6 +2,8 @@ class ContactMailer < ApplicationMailer
   def build(contact)
     @contact = contact
     mail({
+      from: @contact.email,
+      to: 'admin@classpert.com',
       subject: @contact.subject || "New contact",
       reply_to: @contact.email
     })
