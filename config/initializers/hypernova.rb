5db963b928987a3697db618dc1c17f1cf533ffd6
Felipe Jordão [codextremist] <felipe.japm@gmail.com>
2019-08-13 14:43:45 -0300

    Add support to hypernova on docker

--- config/initializers/hypernova.rb
+++ config/initializers/hypernova.rb
@@ -10,8 +10,8 @@ end
 Hypernova.add_plugin!(HypernovaPlugin.new)
 
 Hypernova.configure do |config|
-  config.host = "127.0.0.1"
-  config.port = 7777
+  config.host = ENV.fetch('HYPERNOVA_HOST') { "127.0.0.1" }
+  config.port = ENV.fetch('HYPERNOVA_PORT') { 7777 }
   config.open_timeout = 1
   config.timeout = 2
 end
