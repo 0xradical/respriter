91b1abf9b5a2731dddad47e69d7781906dadbd34
Dalton <dalthon@dlt.local>
2019-08-21 13:32:55 -0300

    Adds tooling for managing production and staging data

--- database/src/data/api/debug.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO api.user_accounts  (email, encrypted_password) VALUES ('user@lorem.com',  'carai');
-INSERT INTO api.admin_accounts (email, encrypted_password) VALUES ('admin@lorem.com', 'carai');
