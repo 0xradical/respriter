12a888c176685cbd4106a64f4dc7a41944bd2860
Thiago Brandão <thiagobrandam@gmail.com>
2019-09-19 08:09:54 -0300

    Add username column to profiles

--- database/src/tables/app/profiles.sql
+++ database/src/tables/app/profiles.sql
@@ -1,6 +1,7 @@
 CREATE TABLE app.profiles (
   id              bigserial PRIMARY KEY,
   name            varchar,
+  username        varchar,
   date_of_birth   date,
   avatar          varchar,
   user_account_id bigint    REFERENCES app.user_accounts(id) ON DELETE CASCADE,
