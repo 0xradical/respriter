ee6cb22b28c3d2ce7a493789f1ac22a9daf0019c
Felipe Jordão <felipe.japm@gmail.com>
2019-10-09 23:57:31 -0300

    Bump elements

--- config/application.rb
+++ config/application.rb
@@ -47,7 +47,7 @@ module App
 
     Elements.configure do |elements_config|
       elements_config.asset_host    = ENV.fetch('ELEMENTS_ASSET_HOST')    { 'https://elements-prd.classpert.com' }
-      elements_config.asset_version = '4.11.1'
+      elements_config.asset_version = '4.12.0'
     end
 
     # Settings in config/environments/* take precedence over those specified here.
