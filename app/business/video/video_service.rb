6d6ca18b0c1905c0785f1a4da11efbec62dd946d
Dalton <dalthon@dlt.local>
2019-10-17 13:48:38 +0200

    Changes how video service works

--- app/business/video/video_service.rb
+++ app/business/video/video_service.rb
@@ -1,21 +1,17 @@
 class Video::VideoService
 
   def self.call(params)
-    url = "#{video_url params.provider, params.id}?#{signed_query_params params.provider, params.id}"
+    url = "#{ video_url path }?#{ signed_query_params params.path }"
     { url: signed(url), embed: params.embed }
   end
 
-  def self.video_url(provider, id)
-    [
-      ENV['VIDEO_SERVICE_HOST'],
-      video_struct.provider,
-      video_struct.id
-    ].join('/')
+  def self.video_url(path)
+    [ ENV['VIDEO_SERVICE_HOST'], path ].join('/')
   end
 
-  def self.signed_query_params(provider, id)
+  def self.signed_query_params(path)
     query_string = "date=#{Time.now.to_i}"
-    query_string + sign("[GET]/#{provider}/#{id}?#{query_string}")
+    query_string + sign("[GET]/#{path}?#{query_string}")
   end
 
   def self.sign(payload)
