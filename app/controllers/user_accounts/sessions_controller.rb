0412a193b40438114286fae8ba63ececac33fa14
Felipe Jordão <felipe.japm@gmail.com>
2019-10-21 17:03:16 -0300

    Refactor UserAccounts::SessionsController

--- app/controllers/user_accounts/sessions_controller.rb
+++ app/controllers/user_accounts/sessions_controller.rb
@@ -3,45 +3,19 @@
 class UserAccounts::SessionsController < Devise::SessionsController
   protect_from_forgery prepend: true
 
-  skip_before_action :verify_signed_out_user, only: :destroy, if: :json_request?
-
-  # before_action :configure_sign_in_params, only: [:create]
-
-  # GET /resource/sign_in
-  def new
-    session[:user_dashboard_redir] = params[:user_dashboard_redir]
-    super
-  end
-
-  # POST /resource/sign_in
-  # def create
-  #   super
-  # end
-
-  # DELETE /resource/sign_out
-  def destroy
-    sign_out :user_account
-    destroy_jwt_session
-    flash.delete :alert
-    respond_to do |format|
-      format.json { head :ok }
-      format.any(*navigational_formats) { redirect_to after_sign_out_path_for(:user_account) }
-    end
-  end
+  before_action  :destroy_api_session,        only: :destroy
+  before_action  :set_user_dashboard_redir,   only: :new
 
   protected
 
-  def json_request?
-    request.format.json?
+  def set_user_dashboard_redir
+    session[:user_dashboard_redir] = params[:user_dashboard_redir]
   end
 
-  def destroy_jwt_session
+  def destroy_api_session
     session[:current_user_jwt] = nil
-    cookies.delete :_jwt, domain: :all
+    cookies.delete :_jwt,         domain: :all
+    cookies.delete :_csrf_token,  domain: :all
   end
 
-  # If you have extra params to permit, append them to the sanitizer.
-  # def configure_sign_in_params
-  #   devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
-  # end
 end
