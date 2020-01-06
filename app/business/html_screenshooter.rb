643a286fb6799395966a9f49e2346353039fbd49
Thiago Brandão <thiagobrandam@gmail.com>
2020-01-06 13:50:16 -0300

    Raise error if no key set

--- app/business/html_screenshooter.rb
+++ app/business/html_screenshooter.rb
@@ -1,24 +1,36 @@
 class HTMLScreenshooter
-  def capture(url, format='png', options={})
+  def capture(url, format = 'png', options = {})
     urlbox_apikey = ENV['URL_BOX_API_KEY']
 
+    raise 'URL_BOX_API_KEY not set' unless urlbox_apikey.present?
+
     query = {
-      :url         => url, # required - the url you want to screenshot
-      :force       => options[:force], # optional - boolean - whether you want to generate a new screenshot rather than receive a previously cached one - this also overwrites the previously cached image
-      :full_page   => options[:full_page], # optional - boolean - return a screenshot of the full screen
-      :thumb_width => options[:thumb_width], # optional - number - thumbnail the resulting screenshot using this width in pixels
-      :width       => options[:width], # optional - number - set viewport width to use (in pixels)
-      :height      => options[:height], # optional - number - set viewport height to use (in pixels)
-      :quality     => options[:quality] # optional - number (0-100) - set quality of the screenshot
+      url: url,
+      # required - the url you want to screenshot
+      force: options[:force],
+      # optional - boolean - whether you want to generate a new screenshot rather than receive a previously cached one - this also overwrites the previously cached image
+      full_page: options[:full_page],
+      # optional - boolean - return a screenshot of the full screen
+      thumb_width: options[:thumb_width],
+      # optional - number - thumbnail the resulting screenshot using this width in pixels
+      width: options[:width],
+      # optional - number - set viewport width to use (in pixels)
+      height: options[:height],
+      # optional - number - set viewport height to use (in pixels)
+      quality: options[:quality] # optional - number (0-100) - set quality of the screenshot
     }
 
-    query_string = query.
-      sort_by {|s| s[0].to_s }.
-      select {|s| s[1] }.
-      map {|s| s.map {|v| encode_uri_component(v.to_s) }.join('=') }.
-      join('&')
+    query_string =
+      query.sort_by { |s| s[0].to_s }.select { |s| s[1] }.map do |s|
+        s.map { |v| encode_uri_component(v.to_s) }.join('=')
+      end.join('&')
 
-    response = Net::HTTP.get(URI("https://api.urlbox.io/v1/#{urlbox_apikey}/#{format}?#{query_string}"))
+    response =
+      Net::HTTP.get(
+        URI(
+          "https://api.urlbox.io/v1/#{urlbox_apikey}/#{format}?#{query_string}"
+        )
+      )
 
     tempfile = Tempfile.new([SecureRandom.uuid, '.png'])
     tempfile.binmode
