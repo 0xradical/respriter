c727557bd6a5c4eac98d55aa09e996b02490b3fe
Thiago Brandão <thiagobrandam@gmail.com>
2020-01-31 22:04:26 -0300

    Unify vcard

---
+++ app/assets/vue/apps/vcard/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <vcard :course="course"> </vcard>
+</template>
+
+<script>
+  import CourseCardVertical from "components/CourseCardVertical.vue";
+
+  export default {
+    name: "VCard",
+    props: {
+      course: {
+        type: Object,
+        required: true
+      },
+      locale: {
+        type: String,
+        required: false,
+        default: "en"
+      }
+    },
+    components: {
+      vcard: CourseCardVertical
+    }
+  };
+</script>
