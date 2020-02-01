bdbe4b1635e02d028beeff2583dc84df7df4cd35
Felipe Jordão <felipe.japm@gmail.com>
2020-02-01 10:57:14 -0300

    Bump to element v5.0.0-pre

--- config/application.rb
+++ config/application.rb
@@ -64,7 +64,7 @@ module App
         ENV.fetch('ELEMENTS_ASSET_HOST') do
           'https://elements-prd.classpert.com'
         end
-      elements_config.asset_version = '5.0.0-beta.12'
+      elements_config.asset_version = '5.0.0-pre'
     end
 
     config.action_controller.forgery_protection_origin_check = false
