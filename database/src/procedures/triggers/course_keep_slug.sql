0f8e663a813d68ef803400b4ae9ffdb08a514196
Dalton <dalthon@dlt.local>
2020-02-03 17:03:01 -0300

    Fix courses defaults

--- database/src/procedures/triggers/course_keep_slug.sql
+++ database/src/procedures/triggers/course_keep_slug.sql
@@ -1,5 +1,9 @@
 CREATE FUNCTION triggers.course_keep_slug() RETURNS trigger AS $$
 BEGIN
+  IF (NEW.published = false) THEN
+    RETURN NEW;
+  END IF;
+
   INSERT INTO app.slug_histories (
     course_id, slug
   ) VALUES (
