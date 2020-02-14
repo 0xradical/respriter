e2fd5b9770b9f2fa9342890c5593609ea1a70821
Thiago Brandão <thiagobrandam@gmail.com>
2020-02-14 18:50:21 -0300

    Update packages

--- config/application.rb
+++ config/application.rb
@@ -64,7 +64,7 @@ module App
         ENV.fetch('ELEMENTS_ASSET_HOST') do
           'https://elements-prd.classpert.com'
         end
-      elements_config.asset_version = '5.1.3'
+      elements_config.asset_version = '5.1.4'
     end
 
     config.action_controller.forgery_protection_origin_check = false
