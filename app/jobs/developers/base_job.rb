4cd8584cde3139221f201aca2b8f4cbbb023ad25
Thiago Brandão <thiagobrandam@gmail.com>
2020-03-11 14:31:27 -0300

    Inject Classpert Bot UA

--- app/jobs/developers/base_job.rb
+++ app/jobs/developers/base_job.rb
@@ -1,5 +1,18 @@
 module Developers
   class BaseJob < Que::Job
+    CLASSPERT_BOT_UA = -> (token) { "Napoleon Crawler (token: #{token})" }
+    LOGGER = Logger.new(STDOUT).tap do |logger|
+      logger.formatter  = proc do |severity, datetime, progname, msg|
+        "#{msg}\n"
+      end
+    end
+
+    self.priority = 100
+    class << self
+      attr_accessor :session_id,
+                    :service_name,
+                    :user_agent_token
+    end
 
     def get_response(url, redirections = 0)
       Net::HTTP.start(
@@ -8,6 +21,9 @@ module Developers
         use_ssl: url.scheme == 'https', open_timeout: 10, read_timeout: 10
       ) do |http|
         request = Net::HTTP::Get.new(url)
+        if self.class.user_agent_token
+          request['User-Agent'] = CLASSPERT_BOT_UA.(self.class.user_agent_token)
+        end
         response = http.request(request)
 
         if !response.code.to_i.in?([301, 302])
@@ -22,6 +38,24 @@ module Developers
       end
     rescue Net::OpenTimeout
       raise "Timeout while trying to access #{url}"
+    end
+
+    def self.log(message, level = :info)
+      LOGGER.public_send(
+        level,
+        {
+          id: SecureRandom.uuid,
+          ps: {
+            id: self.session_id,
+            name: self.service_name
+          },
+          payload: message
+        }.to_json
+      )
+    end
+
+    def log(message, level = :info)
+      self.class.log(message, level)
     end
   end
 end
