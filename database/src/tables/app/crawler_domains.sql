e668042e754f76904d071a2d03876872442f5067
Thiago Brandão <thiagobrandam@gmail.com>
2020-04-03 23:39:03 -0300

    Fix database

--- database/src/tables/app/crawler_domains.sql
+++ database/src/tables/app/crawler_domains.sql
@@ -4,10 +4,10 @@ CREATE TABLE app.crawler_domains (
   authority_confirmation_status app.authority_confirmation_status DEFAULT    'unconfirmed'                               NOT NULL,
   authority_confirmation_token  varchar,
   authority_confirmation_method app.authority_confirmation_method DEFAULT    'dns'                                       NOT NULL,
-  authority_confirmation_salt   varchar,
   created_at                    timestamptz                       DEFAULT    NOW()                                       NOT NULL,
   updated_at                    timestamptz                       DEFAULT    NOW()                                       NOT NULL,
   domain                        varchar                                                                                  NOT NULL,
+  authority_confirmation_salt   varchar
 
   CONSTRAINT domain__must_be_a_domain CHECK ( domain ~ '^([a-z0-9\-\_]+\.)+[a-z]+$' )
 );
