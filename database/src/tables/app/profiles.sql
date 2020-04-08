d6f784ee8bdc72fdac57dd3e741e01feab6f5cc8
Dalton <dalthon@dlt.local>
2020-04-08 07:57:03 -0300

    Fix username _username constraint bug

--- database/src/tables/app/profiles.sql
+++ database/src/tables/app/profiles.sql
@@ -1,15 +1,15 @@
 CREATE TABLE app.profiles (
-  id              uuid DEFAULT public.uuid_generate_v4() PRIMARY KEY,
-  name            varchar,
-  username        app.username,
-  _username       varchar CONSTRAINT username_format CHECK (username ~* '^\w{5,15}$'),
-  username_changed_at TIMESTAMPTZ,
-  date_of_birth   date,
-  oauth_avatar_url varchar,
+  id                  uuid          DEFAULT public.uuid_generate_v4() PRIMARY KEY,
+  name                varchar,
+  username            app.username,
+  _username           varchar       CONSTRAINT username_format CHECK (_username ~* '^\w{5,15}$'),
+  username_changed_at timestamptz,
+  date_of_birth       date,
+  oauth_avatar_url    varchar,
   uploaded_avatar_url varchar,
-  user_account_id bigint    REFERENCES app.user_accounts(id) ON DELETE CASCADE,
-  interests       text[]    DEFAULT '{}'::text[],
-  preferences     jsonb     DEFAULT '{}'::jsonb,
-  created_at      TIMESTAMPTZ DEFAULT NOW() NOT NULL,
-  updated_at      TIMESTAMPTZ DEFAULT NOW() NOT NULL
+  user_account_id     bigint        REFERENCES app.user_accounts(id) ON DELETE CASCADE,
+  interests           text[]        DEFAULT '{}'::text[],
+  preferences         jsonb         DEFAULT '{}'::jsonb,
+  created_at          timestamptz   DEFAULT NOW() NOT NULL,
+  updated_at          timestamptz   DEFAULT NOW() NOT NULL
 );
