58836d9b9922295ae275e11c2f45cd2899d70d45
Dalton <dalthon@dlt.local>
2020-04-17 09:34:14 -0300

    Increased index tool boost a lot more

--- app/models/search/course_search.rb
+++ app/models/search/course_search.rb
@@ -123,7 +123,7 @@ module Search
           term: {
             from_index_tool: {
               value: true,
-              boost: 2.5
+              boost: 5
             }
           }
         }
