d301dbf7a483e13436d4087c992ed5a5d35645d8
Felipe Jordão <felipe.japm@gmail.com>
2020-05-21 16:12:21 -0300

    Change i18n refs

--- developers-dashboard/src/config/locales.js
+++ developers-dashboard/src/config/locales.js
@@ -1,24 +1,24 @@
 const $ = require("deepmerge");
 
 // en
-const enPublic = require("../locales/en/en.yml");
-const enBase = require("../locales/en/en.base.yml");
-const enUser = require("../locales/en/en.user.yml");
-const enTags = require("../locales/en/en.tags.yml");
+const enPublic = require("../locales/en/web-app.yml");
+const enBase = require("../locales/en/rails.yml");
+const enUser = require("../locales/en/user.yml");
+const enTags = require("../locales/en/tags.yml");
 const en = $.all([enBase, enUser, enTags, enPublic]);
 
 // es
-const esPublic = require("../locales/es/es.yml");
-const esBase = require("../locales/es/es.base.yml");
-const esUser = require("../locales/es/es.user.yml");
-const esTags = require("../locales/es/es.tags.yml");
+const esPublic = require("../locales/es/web-app.yml");
+const esBase = require("../locales/es/rails.yml");
+const esUser = require("../locales/es/user.yml");
+const esTags = require("../locales/es/tags.yml");
 const es = $.all([esBase, esUser, esTags, esPublic]);
 
 // pt-BR
-const ptBRPublic = require("../locales/pt-BR/pt-BR.yml");
-const ptBRBase = require("../locales/pt-BR/pt-BR.base.yml");
-const ptBRUser = require("../locales/pt-BR/pt-BR.user.yml");
-const ptBRTags = require("../locales/pt-BR/pt-BR.tags.yml");
+const ptBRPublic = require("../locales/pt-BR/web-app.yml");
+const ptBRBase = require("../locales/pt-BR/rails.yml");
+const ptBRUser = require("../locales/pt-BR/user.yml");
+const ptBRTags = require("../locales/pt-BR/tags.yml");
 const ptBR = $.all([ptBRBase, ptBRUser, ptBRTags, ptBRPublic]);
 
 const all = $.all([en, es, ptBR]);
--- user-dashboard/src/config/locales.js
+++ user-dashboard/src/config/locales.js
@@ -1,27 +1,27 @@
 const $ = require("deepmerge");
 
 // en
-const enPublic = require("../locales/en/en.yml");
-const enBase = require("../locales/en/en.base.yml");
-const enUser = require("../locales/en/en.user.yml");
-const enTags = require("../locales/en/en.tags.yml");
-const enDbs = require("../locales/en/en.db.yml");
+const enPublic = require("../locales/en/web-app.yml");
+const enBase = require("../locales/en/rails.yml");
+const enUser = require("../locales/en/user.yml");
+const enTags = require("../locales/en/tags.yml");
+const enDbs = require("../locales/en/db.yml");
 const en = $.all([enBase, enUser, enTags, enPublic, enDbs]);
 
 // es
-const esPublic = require("../locales/es/es.yml");
-const esBase = require("../locales/es/es.base.yml");
-const esUser = require("../locales/es/es.user.yml");
-const esTags = require("../locales/es/es.tags.yml");
-const esDbs = require("../locales/es/es.db.yml");
+const esPublic = require("../locales/es/web-app.yml");
+const esBase = require("../locales/es/rails.yml");
+const esUser = require("../locales/es/user.yml");
+const esTags = require("../locales/es/tags.yml");
+const esDbs = require("../locales/es/db.yml");
 const es = $.all([esBase, esUser, esTags, esPublic, esDbs]);
 
 // pt-BR
-const ptBRPublic = require("../locales/pt-BR/pt-BR.yml");
-const ptBRBase = require("../locales/pt-BR/pt-BR.base.yml");
-const ptBRUser = require("../locales/pt-BR/pt-BR.user.yml");
-const ptBRTags = require("../locales/pt-BR/pt-BR.tags.yml");
-const ptBRTDbs = require("../locales/pt-BR/pt-BR.db.yml");
+const ptBRPublic = require("../locales/pt-BR/web-app.yml");
+const ptBRBase = require("../locales/pt-BR/rails.yml");
+const ptBRUser = require("../locales/pt-BR/user.yml");
+const ptBRTags = require("../locales/pt-BR/tags.yml");
+const ptBRTDbs = require("../locales/pt-BR/db.yml");
 const ptBR = $.all([ptBRBase, ptBRUser, ptBRTags, ptBRPublic, ptBRTDbs]);
 
 const all = $.all([en, es, ptBR]);
