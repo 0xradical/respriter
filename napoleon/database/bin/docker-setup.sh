5954382628d766a984fda58ebfbdc17aff4a1285
Dalton <dalthon@dlt.local>
2020-05-26 15:03:18 -0300

    Fix small script issues for napoleon

--- napoleon/database/bin/docker-setup.sh
+++ napoleon/database/bin/docker-setup.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-query_paramsless_url=$(echo $POSTGRES_URL | cut -d\? -f1)
-cat /database/db/structure.sql.env | envsubst | psql $query_paramsless_url
+cat /database/db/structure.sql.env | envsubst | ( PGPASSWORD=`./bin/parse_uri $POSTGRES_URL password` psql -U `./bin/parse_uri $POSTGRES_URL user` -d `./bin/parse_uri $POSTGRES_URL path` )
 
 if [[ -f "/database/db/${DB_ENV}_seed.sql" ]]; then
   echo "Running seeds at /database/db/${DB_ENV}_seed.sql"
