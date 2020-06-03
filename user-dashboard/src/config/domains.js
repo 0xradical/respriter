f148d902a61ba90848bdaa101f5cc9292e4a4803
Thiago Brandão <thiagobrandam@gmail.com>
2020-06-03 16:45:11 -0300

    Add Japanese URL

--- user-dashboard/src/config/domains.js
+++ user-dashboard/src/config/domains.js
@@ -6,7 +6,8 @@ let url = parse(env.webAppURL);
 const domains = {
   en: `${url.protocol}//${url.host}`,
   "pt-BR": `${url.protocol}//pt-br.${url.host}`,
-  es: `${url.protocol}//es.${url.host}`
+  es: `${url.protocol}//es.${url.host}`,
+  ja: `${url.protocol}//ja.${url.host}`
 };
 
 export function webAppDomainURL(domain, path) {
