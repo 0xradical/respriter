5ee2748115dd42d359246d241ec44328cc33b856
Felipe Jordão <felipe.japm@gmail.com>
2020-06-17 12:06:39 -0300

    Try calling indexable method when nil

--- web-app/app/controllers/courses_controller.rb
+++ web-app/app/controllers/courses_controller.rb
@@ -19,7 +19,7 @@ class CoursesController < ApplicationController
     @provider = Provider.find_by!(slug: params[:provider])
     @course   = @provider.courses.published.find_by(slug: params[:course])
 
-    response.set_header('X-Robots-Tag', 'noindex') unless @course.indexable_by_robots_for_locale?
+    response.set_header('X-Robots-Tag', 'noindex') unless @course&.indexable_by_robots_for_locale?
 
     unless @course
       redirected_course = Course
