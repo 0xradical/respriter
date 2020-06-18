e1c88457b61714458d94c5b92229b9f9810087d2
Thiago Brandão <thiagobrandam@gmail.com>
2020-06-18 16:32:11 -0300

    Use unglob in builder

--- spec/utils_spec.rb
+++ spec/utils_spec.rb
@@ -2,8 +2,16 @@
 
 RSpec.describe 'Respriter::Utils' do
   describe '.unglob' do
-    it 'unglobs brace-based globs' do
-      expect(Respriter::Utils.unglob('a{b,c}{d,e,f}')).to eq(%w[abd abe abf acd ace acf])
+    context 'when there is glob' do
+      it 'unglobs brace-based globs' do
+        expect(Respriter::Utils.unglob('a{b,c}{d,e,f}')).to eq(%w[abd abe abf acd ace acf])
+      end
+    end
+
+    context 'when there is no glob' do
+      it 'does nothing' do
+        expect(Respriter::Utils.unglob('abd')).to eq(['abd'])
+      end
     end
   end
 end
