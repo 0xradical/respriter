28cb2b7f311a3a005ec876315ebb7781c8cf0cf6
Thiago Brandão <thiagobrandam@gmail.com>
2020-06-23 22:21:18 -0300

    Add code deploy permissions to code pipeline

--- terraform/main.tf
+++ terraform/main.tf
@@ -474,15 +474,19 @@ resource "aws_iam_role_policy" "codepipeline_policy" {
     {
       "Effect":"Allow",
       "Action": [
-        "s3:GetObject",
-        "s3:GetObjectVersion",
-        "s3:GetBucketVersioning",
-        "s3:PutObject"
+        "s3:*"
       ],
       "Resource": [
         "${aws_s3_bucket.codepipeline_bucket.arn}",
         "${aws_s3_bucket.codepipeline_bucket.arn}/*"
       ]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "codedeploy:*"
+      ],
+      "Resource": "*"
     }
   ]
 }
