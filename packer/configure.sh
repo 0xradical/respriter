9166484928d971afeae46388a2d91ee638aaf0d8
Thiago Brandão <thiagobrandam@gmail.com>
2020-06-24 15:51:24 -0300

    change machine

--- packer/configure.sh
+++ packer/configure.sh
@@ -1,17 +1,17 @@
+sudo apt-get dist-upgrade
 sudo apt-get update
 sudo apt-get -y install build-essential patch ruby-dev zlib1g-dev liblzma-dev ruby wget
-wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
-chmod +x ./install
-sudo ./install auto
 mkdir /home/ubuntu/.ruby
 mkdir /home/ubuntu/.bundle
 mkdir /home/ubuntu/.nvm
-export GEM_HOME=/home/ubuntu/.ruby
-export PATH="$PATH:/home/ubuntu/.ruby/bin"
-echo 'export GEM_HOME=/home/ubuntu/.ruby' >> /home/ubuntu/.bashrc
-echo 'export PATH=\"$PATH:/home/ubuntu/.ruby/bin\"' >> /home/ubuntu/.bashrc
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-. /home/ubuntu/.bashrc
-. /home/ubuntu/.nvm/nvm.sh
+echo 'export GEM_HOME=/home/ubuntu/.ruby' >> /home/ubuntu/.profile
+echo 'export PATH="$PATH:/home/ubuntu/.ruby/bin"' >> /home/ubuntu/.profile
+wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | PROFILE=/home/ubuntu/.profile bash
+source /home/ubuntu/.profile
 gem install bundler
+gem install nokogiri
 nvm install 12.18.1
+nvm alias default 12.18.1
+wget https://aws-codedeploy-us-east-1.s3.us-east-1.amazonaws.com/latest/install
+chmod +x ./install
+sudo ./install auto
