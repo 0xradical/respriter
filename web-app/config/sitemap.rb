06ebb524369ccd7db3f8a1e1103294f1c0b0fd76
Felipe Jordão <felipe.japm@gmail.com>
2020-06-25 17:52:59 -0300

    Upload generated sitemaps to S3 * Update the robots.txt from each subdomain, with a ref to the new sitemap_index url

--- web-app/config/sitemap.rb
+++ web-app/config/sitemap.rb
@@ -1,20 +1,35 @@
+SitemapGenerator::Sitemap.adapter = SitemapGenerator::AwsSdkAdapter.new(
+  ENV['AWS_S3_BUCKET_NAME'],
+  {
+    aws_endpoint:          ENV.fetch('AWS_S3_ENDPOINT'),
+    aws_access_key_id:     ENV.fetch('AWS_ACCESS_KEY_ID'),
+    aws_secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
+    aws_region:            ENV.fetch('AWS_S3_REGION')
+  }
+)
+
 I18nHost.new('classpert.com').each do |locale, host|
   SitemapGenerator::Sitemap.default_host  = "https://#{host}"
+  SitemapGenerator::Sitemap.sitemaps_host = "https://cdn.classpert.com"
+  SitemapGenerator::Sitemap.public_path   = 'tmp/'
   SitemapGenerator::Sitemap.sitemaps_path = "sitemaps/#{locale}"
-  SitemapGenerator::Sitemap.create_index  = :auto
-  SitemapGenerator::Sitemap.create include_root: false do
+  SitemapGenerator::Sitemap.create_index  = true
+  SitemapGenerator::Sitemap.compress      = :all_but_first
+
+  SitemapGenerator::Sitemap.create  include_root: false do
 
     add(root_path,                 { changefreq: 'weekly',  priority: 1 })
     add(privacy_policy_path,       { changefreq: 'yearly',  priority: 0.5})
     add(terms_and_conditions_path, { changefreq: 'yearly',  priority: 0.5})
     add(courses_path,              { changefreq: 'daily',   priority: 0.5})
 
-    # Blog
+    # Blog - Index
     add(posts_path, {
       changefreq: 'weekly',
       priority: 0.9,
       lastmod: Post.locale(I18nHelper.sanitize_locale(locale)).order(published_at: :desc).first&.published_at&.strftime('%Y-%m-%d')
     })
+    # Blog - Post
     Post.locale(I18nHelper.sanitize_locale(locale)).published.each do |post|
       add(post_path(post.slug), { changefreq: 'monthly', priority: 0.9,  lastmod: post.content_changed_at })
     end
