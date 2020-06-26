b5aa2c4da93ae0dcaf5d173f8bbf62ffb5a28747
Thiago Brandão <thiagobrandam@gmail.com>
2020-06-26 14:38:31 -0300

    Organize server code

--- server.js
+++ server.js
@@ -1,126 +1,28 @@
-const fs = require("fs");
-const path = require("path");
 const express = require("express");
-const cors = require("cors");
-const { compose, toPairs, map, reduce, join, split } = require("ramda");
-const rootDir = path.resolve();
-const distDir = path.resolve(rootDir, "dist");
-const defsDir = scope => path.resolve(distDir, scope, "defs");
-const symbolsDir = scope => path.resolve(distDir, scope, "symbols");
-const defsResolve = scope => def => path.resolve(defsDir(scope), def);
-const symbolsResolve = scope => symbol =>
-  path.resolve(symbolsDir(scope), symbol);
-
-const corsOptions = {};
-if (process.env.SPRITE_CORS_ORIGIN) {
-  corsOptions.origin = process.env.SPRITE_CORS_ORIGIN;
-}
-
-const dependencies = fs
-  .readdirSync(path.resolve(distDir))
-  .reduce((acc, scopedFolder) => {
-    return {
-      ...acc,
-      [scopedFolder]: JSON.parse(
-        fs.readFileSync(
-          path.resolve(distDir, scopedFolder, "dependencies.json"),
-          {
-            encoding: "utf8",
-            flag: "r"
-          }
-        )
-      )
-    };
-  }, {});
-
-console.log(dependencies);
-
+const cors = require("./server/cors");
+const manifest = require("./server/manifest");
+const { noCache, log } = require("./server/utils");
+const { builder } = require("./server/processor");
+const processor = builder(manifest);
 const app = express();
-const svg = (symbols, defs) => {
-  return `<svg xmlns="http://www.w3.org/2000/svg"><defs>${
-    defs && defs.join("")
-  }</defs>${symbols && symbols.join("")}</svg>`;
-};
-const symbolDeps = scope => symbol => {
-  return dependencies[scope][symbol] || [];
-};
-const nodePromise = nodePath => {
-  return new Promise((resolve, _reject) => {
-    fs.readFile(nodePath, "utf8", (err, data) => {
-      if (err) {
-        console.log("ERROR", err);
-        resolve("");
-      } else {
-        resolve(data);
-      }
-    });
-  });
-};
 
-const log = payload => {
-  console.log(payload);
-  return payload;
-};
+log(manifest);
 
-const spritePromise = scope => params => {
-  try {
-    return compose(
-      // collect promises
-      ({ symbols, defs }) => symbols.then(s => defs.then(d => svg(s, d))),
-      // construct promises of symbols and defs
-      ({ symbols, defs }) => ({
-        symbols: Promise.all(
-          map(compose(nodePromise, symbolsResolve(scope)), symbols)
-        ),
-        defs: Promise.all(map(compose(nodePromise, defsResolve(scope)), defs))
-      }),
-      // log,
-      // add dependencies of symbols
-      reduce(
-        (acc, s) => ({
-          symbols: [...acc.symbols, s],
-          defs: [...acc.defs, ...symbolDeps(scope)(s)]
-        }),
-        { symbols: [], defs: [] }
-      ),
-      // log,
-      // discard keys
-      reduce((acc, [_, o]) => [...acc, ...o], []),
-      // log,
-      // flattenize everyone into sprite format
-      map(([k, o]) => [k, map(o => join("-", [k, o]), split(",", o))]),
-      // transform globbed scopes
-      map(([k, o]) =>
-        o === "*"
-          ? [
-              k,
-              fs
-                .readdirSync(symbolsDir(scope))
-                .reduce((a, s) => {
-                  var m = new RegExp(`^${k}-(.*)`).exec(s);
-                  return m ? [...a, m[1]] : a;
-                }, [])
-                .join(",")
-            ]
-          : [k, o]
-      ),
-      // log,
-      // transform query into object
-      toPairs
-    )(params);
-  } catch (error) {
-    console.log(error);
-    return Promise.reject("");
-  }
-};
+app.get("/", function (_req, res) {
+  noCache(res);
+  res.status(404);
+  res.end();
+});
 
-app.get("/healthcheck", function (req, res) {
+app.get("/healthcheck", function (_req, res) {
+  noCache(res);
   res.status(200);
   res.end();
 });
 
 app.get("/test/:version", function (req, res) {
   const version = req.params.version;
+  noCache(res);
 
   res.send(`
   <!DOCTYPE html>
@@ -158,19 +60,18 @@ app.get("/test/:version", function (req, res) {
   `);
 });
 
-app.get("/:version", cors(corsOptions), function (req, res) {
-  const version = req.params.version;
-
-  spritePromise(version)(req.query)
+app.get("/:version", cors, function (req, res) {
+  processor(version)(req.query)
     .then(svgPayload => {
       res.contentType("image/svg");
       res.send(svgPayload);
     })
-    .catch(() => res.status(500));
+    .catch(error => {
+      console.log(error);
+      noCache(res);
+      res.status(500);
+      res.end();
+    });
 });
 
-const server = app.listen(
-  process.env.NODE_PORT || 8080,
-  "0.0.0.0",
-  function () {}
-);
+app.listen(process.env.NODE_PORT || 8080, "0.0.0.0", function () {});
---
+++ server/processor.js
@@ -0,0 +1,61 @@
+const fs = require("fs");
+const { compose, toPairs, map, reduce, join, split } = require("ramda");
+const { svg, log, dataReadAsync } = require("./utils");
+const { resolveEntry, resolveFamily } = require("./resolvers");
+
+const builder = manifest => scope => params => {
+  try {
+    return compose(
+      // collect promises
+      ({ symbols, defs }) => symbols.then(s => defs.then(d => svg(s, d))),
+      // construct promises of symbols and defs
+      ({ symbols, defs }) => ({
+        symbols: Promise.all(
+          map(compose(dataReadAsync, resolveEntry("symbols")(scope)), symbols)
+        ),
+        defs: Promise.all(
+          map(compose(dataReadAsync, resolveEntry("defs")(scope)), defs)
+        )
+      }),
+      // log,
+      // add dependencies of symbols
+      reduce(
+        (acc, s) => ({
+          symbols: [...acc.symbols, s],
+          defs: [...acc.defs, ...manifest.fetch(scope)(s)]
+        }),
+        { symbols: [], defs: [] }
+      ),
+      // log,
+      // discard keys
+      reduce((acc, [_, o]) => [...acc, ...o], []),
+      // log,
+      // flattenize everyone into sprite format
+      map(([k, o]) => [k, map(o => join("-", [k, o]), split(",", o))]),
+      // log,
+      // transform globbed scopes
+      map(([k, o]) =>
+        o === "*"
+          ? [
+              k,
+              fs
+                .readdirSync(resolveFamily("symbols")(scope)())
+                .reduce((a, s) => {
+                  var m = new RegExp(`^${k}-(.*)`).exec(s);
+                  return m ? [...a, m[1]] : a;
+                }, [])
+                .join(",")
+            ]
+          : [k, o]
+      ),
+      // log,
+      // transform query into object
+      toPairs
+    )(params);
+  } catch (error) {
+    log(error);
+    return Promise.reject("");
+  }
+};
+
+module.exports = { builder };
