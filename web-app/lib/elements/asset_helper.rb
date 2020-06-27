c502e8955287e17e98436b5f36e4d70c1a6e1752
Felipe Jordão <felipe.japm@gmail.com>
2020-06-26 21:38:40 -0300

    Allow "per-page" call to versions of Elements
    Currently, only a single global version of Elements is
    supported for the entire app. This commit introduces a local "per-page"
    way to invoke elements assets, overriding the global configuration when
    called. To invoke a different version on any given page, just call:
    
    ```ruby
    <% content_for :elements_version, '[version]' %>
    ```
    
    Calling this method is a handy way to call all Elements assets defined on
    `global.html.erb` under version `[version]`
    
    However, if you can fully control how assets are required. Let's say
    for example  that the global configuration of Elements is pointing
    to version `8.3.3` which has only three different assets:
    `icon-font.css`, `bundle.css` and `bundle.js`. Version
    9.0.0 introduces new files: `atomic.css` and `components.js`. You can
    call them on your local view
    
    ```ruby
    <% content_for :elements_css do %>
        <%= elements_stylesheet_link_tag('icon-font', 'bundle', 'atomics', version: '9.0.0', skip_pipeline: true) %>
    <% end %>
    
    <% content_for :elements_js do %>
      <%= elements_javascript_include_tag('bundle', 'components.js', version: '9.0.0', async: false, skip_pipeline: true) %>
    <% end %>
    ```
    You can also call custom sprites!
    
    ```ruby
    <% content_for :elements_sprites do %>
      <div data-svg-sprite="<%= elements_asset_path('svgs/sprites/all.svg', version: '9.0.0' ) %>"></div>
    <% end %>
    ```

--- web-app/lib/elements/asset_helper.rb
+++ web-app/lib/elements/asset_helper.rb
@@ -4,21 +4,30 @@ module Elements
     include ActionView::Helpers::AssetTagHelper
 
     def elements_javascript_include_tag(*sources)
-      sources.extract_options!
-      sources.map { |source| source.prepend("/#{Elements.asset_version}/").squeeze('/') }
-      javascript_include_tag(*sources, host: Elements.asset_host, skip_pipeline: true)
+      options = sources.extract_options!
+      version = get_version(options.extract!(:version))
+      sources.map { |source| source.prepend("/#{version}/").squeeze('/') }
+      javascript_include_tag(*sources, {host: Elements.asset_host}.merge(options))
     end
 
     def elements_stylesheet_link_tag(*sources)
-      sources.extract_options!
-      sources.map { |source| source.prepend("/#{Elements.asset_version}/").squeeze('/') }
-      stylesheet_link_tag(*sources, host: Elements.asset_host, skip_pipeline: true)
+      options = sources.extract_options!
+      version = get_version(options.extract!(:version))
+      sources.map { |source| source.prepend("/#{version}/").squeeze('/') }
+      stylesheet_link_tag(*sources, {host: Elements.asset_host}.merge(options))
     end
 
     def elements_asset_path(source, options={})
       source.delete_prefix!('/')
-      source.prepend("#{Elements.asset_host}/#{Elements.asset_version}/")
+      version = get_version(options.extract!(:version))
+      source.prepend("#{Elements.asset_host}/#{version}/")
       asset_path(source, options)
+    end
+
+    private
+
+    def get_version(opts)
+      opts[:version].blank? ? Elements.asset_version : opts[:version]
     end
 
   end
