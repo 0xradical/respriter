f03649a2ceb23d3cb5f5623c0ebd1203a7135fae
Thiago Brandão <thiagobrandam@gmail.com>
2020-07-16 15:31:50 -0300

    Add sid properly to course url

--- web-app/app/business/search_tracker.rb
+++ web-app/app/business/search_tracker.rb
@@ -1,8 +1,14 @@
+# frozen_string_literal: true
+
+require 'addressable'
+
 class SearchTracker
   attr_reader :session_tracker, :search, :action
 
   def initialize(session_tracker, search, action: nil)
-    @session_tracker, @search, @action = session_tracker, search, action
+    @session_tracker = session_tracker
+    @search = search
+    @action = action
   end
 
   def id
@@ -18,34 +24,40 @@ class SearchTracker
     tracked_results = @search.results.deep_dup
     query_param = "?sid=#{id}"
     tracked_results[:data] = tracked_results[:data].map do |result|
-      result[:gateway_path] += query_param
+      gateway_path = result[:gateway_path].presence
+      if gateway_path
+        parsed_path = Addressable::URI.parse(gateway_path)
+        parsed_path.query_values = (parsed_path.query_values || {}).merge({ sid: id })
+        result[:gateway_path] = parsed_path.to_s
+      end
       result
     end
     tracked_results
   end
 
   private
+
   def tracked_action
     {
-      id: id,
-      version: Search::CourseSearch::VERSION,
-      request: {
+      id:           id,
+      version:      Search::CourseSearch::VERSION,
+      request:      {
         query:    @search.query,
         filter:   @search.filter,
         page:     @search.page,
         per_page: @search.per_page,
         order:    @search.order,
-        boost:    @search.boost,
+        boost:    @search.boost
       },
-      results: {
+      results:      {
         ids:   @search.results[:data].map(&:id),
-        total: @search.results[:meta][:total],
+        total: @search.results[:meta][:total]
       },
       tracked_data: {
         session: @session_tracker.session_payload,
-        cookies: @session_tracker.cookies_payload,
+        cookies: @session_tracker.cookies_payload
       },
-      action: @action
+      action:       @action
     }
   end
 end
