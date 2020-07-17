b2902e58947cfe5ca0a9878b3ba21cfb148a7075
Felipe Jordão <felipe.japm@gmail.com>
2020-07-17 08:15:30 -0300

    Make RackAttack shine with Redis

--- web-app/config/initializers/rack_attack.rb
+++ web-app/config/initializers/rack_attack.rb
@@ -1,51 +1,85 @@
+# frozen_string_literal: true
 class Rack::Attack
 
-  class Request < ::Rack::Request
-    def remote_ip
-      (env['HTTP_X_FORWARDED_FOR'] || request.remote_ip).to_s.scan(/(.*),|\A(.*)\z/).flatten.compact.first
-    end
-    def bot?
-      Browser.new(env['HTTP_USER_AGENT']).bot?
-    end
-  end
+  ALLOW_PERIOD_FOR_TRUSTED_BOTS_IN_DAYS   = ENV.fetch('RACK_ATTACK__ALLOW_PERIOD_FOR_TRUSTED_BOTS_IN_DAYS') { 120 }
+  THROTTLE_LEVELS                         = ENV.fetch('RACK_ATTACK__THROTTLE_LEVELS')                       { 4   }
+  THROTTLE_LIMIT                          = ENV.fetch('RACK_ATTACK__THROTTLE_LIMIT')                        { 150 }
+  THROTTLE_PERIOD                         = ENV.fetch('RACK_ATTACK__THROTTLE_PERIOD_IN_MINUTES')            { 15  }
+  THROTTLE_BAN_PERIOD_FOR_ABUSE_IN_DAYS   = ENV.fetch('RACK_ATTACK__THROTTLE_BAN_PERIOD_FOR_ABUSE_IN_DAYS') { 120 }
+
+  self.cache.store = ActiveSupport::Cache::RedisCacheStore.new({ url: ENV['REDIS_URL'] })
 
   safelist("allowed IPs") do |req|
-    self.cache.read("allow-#{req.remote_ip}")
+    self.cache.read("allow:#{req.remote_ip}")
   end
 
   blocklist("blocked IPs") do |req|
-    self.cache.read("block-#{req.remote_ip}")
+    self.cache.read("block:#{req.remote_ip}")
   end
 
-  throttle("req/ip/humans", :limit => 20, :period => 8.seconds) do |req|
-    req.remote_ip unless req.path.start_with?('/assets')
-  end
+  (1..THROTTLE_LEVELS).each do |level|
 
-  (1..4).each do |level|
-    throttle("req/ip/bots/#{level}", :limit => (150 * level), :period => level.hour) do |req|
-      cache_key = "#{Time.now.to_i/(level.hour)}:req/ip/bots/#{level}:#{req.remote_ip}"
+    limit   = THROTTLE_LIMIT * level
+    period  = (THROTTLE_PERIOD * level).minutes
+
+    throttle("#{limit}req/ip/#{period}s", :limit => limit, :period => period) do |req|
+
+      cache_key = "#{Time.now.to_i/period}:#{limit}req/ip/#{period}s:#{req.remote_ip}"
+
       unless req.path.start_with?('/assets')
-        if req.bot?
-          begin
-            bot_claim = BotClaim.new(req.user_agent, req.remote_ip)
-            self.cache.write("allow-#{bot_claim.ip}", bot_claim.name, Time.now + 3.months) if bot_claim.verified?
-          rescue BotClaim::UnsupportedUserAgent => e
-            Rails.logger.info(e.message)
+        if req.browser.bot?
+          inspector = BotInspector.new(req.remote_ip)
+          if inspector.trusted_claim?
+            self.cache.write(
+              "allow:#{req.remote_ip}", 
+              "ua=#{req.browser.name}&raw_ua=#{req.user_agent}&country=#{req.country}", 
+              Time.now + ALLOW_PERIOD_FOR_TRUSTED_BOTS_IN_DAYS.days
+            )
+            next
           end
-          blacklist_ip_if_abused(cache_key, req.remote_ip, threshold: (150 * level), ban_time: Time.now + (7 * level).days)
-          req.remote_ip
-        else
-          blacklist_ip_if_abused(cache_key, req.remote_ip, threshold: (150 * level), ban_time: Time.now + (7 * level).days)
-          req.remote_ip
         end
+
+        actual_reqs = self.cache.read(cache_key).to_i
+
+        if (actual_reqs >= limit)
+          acc = self.cache.read("throttled:#{req.remote_ip}:#{req.user_agent}:#{req.country}")
+          self.cache.write("throttled:#{req.remote_ip}:#{req.user_agent}:#{req.country}", acc + actual_reqs, Time.now + 1.year)
+
+          max_limit = THROTTLE_LEVELS * THROTTLE_LIMIT
+
+          if actual_reqs >= max_limit
+            self.cache.write(
+              "block:#{req.remote_ip}",
+              "reqs=#{actual_reqs}&ua=#{req.browser.name}&raw_ua=#{req.user_agent}&country=#{req.country}", 
+              (Time.now + THROTTLE_BAN_PERIOD_FOR_ABUSE_IN_DAYS.days)
+            )
+          end
+        end
+
+        req.remote_ip
       end
+
     end
   end
 
-  def self.blacklist_ip_if_abused(key, ip, threshold:, ban_time:)
-    if (self.cache.read(key).to_i > threshold)
-      self.cache.write("block-#{ip}", 1, ban_time)
+  class Request < ::Rack::Request
+
+    def remote_ip
+      (env['HTTP_X_FORWARDED_FOR'] || request.remote_ip).to_s.scan(/(.*),|\A(.*)\z/).flatten.compact.first
     end
+
+    def browser
+      Browser.new(user_agent)
+    end
+
+    def user_agent
+      env['HTTP_USER_AGENT']
+    end
+
+    def country
+      env['HTTP_CF_COUNTRY']
+    end
+
   end
 
 end
--- web-app/lib/tasks/rack_attack.rake
+++ web-app/lib/tasks/rack_attack.rake
@@ -3,8 +3,8 @@ namespace :rack_attack do
   PERIOD_REGEXP = /(?<value>\d*) (?<unit>seconds?|minutes?|hours?|days?|months?|years?)/
 
   namespace :blacklist do
-    desc 'Add IPs to blacklist. Example: add[<ips>,<ban_period>]'
-    task :add, %i[ips ban_period] => %i[environment] do |t,args|
+    desc 'Add IPs to blacklist. Example: add[<ips>,<ban_period>,<desc>]'
+    task :add, %i[ips ban_period desc] => %i[environment] do |t,args|
       ban_period = PERIOD_REGEXP.match(args[:ban_period])
 
       unless ban_period
@@ -13,21 +13,21 @@ namespace :rack_attack do
 
       ban_time = Time.now + ban_period[:value].to_i.send(ban_period[:unit])
       args[:ips].split(',').each do |ip|
-        Rack::Attack.cache.write("block-#{ip}",1,ban_time)
+        Rack::Attack.cache.write("block:#{ip}",args[:desc],ban_time)
       end
     end
 
     desc 'Remove IPs from blacklist. Example: remove[<ips>]'
     task :remove, %i[ips] => %i[environment] do |t,args|
       args[:ips].split(',').each do |ip|
-        Rack::Attack.cache.delete("block-#{ip}")
+        Rack::Attack.cache.delete("block:#{ip}")
       end
     end
   end
 
   namespace :whitelist do
-    desc 'Add IPs to whitelist. Example: add[<ips>,<allow_period>]'
-    task :add, %i[ips allow_period] => %i[environment] do |t,args|
+    desc 'Add IPs to whitelist. Example: add[<ips>,<allow_period>,<desc>]'
+    task :add, %i[ips allow_period desc] => %i[environment] do |t,args|
       allow_period = PERIOD_REGEXP.match(args[:allow_period])
 
       unless allow_period
@@ -36,14 +36,14 @@ namespace :rack_attack do
 
       allow_time = Time.now + allow_period[:value].to_i.send(allow_period[:unit])
       args[:ips].split(',').each do |ip|
-        Rack::Attack.cache.write("allow-#{ip}",1,allow_time)
+        Rack::Attack.cache.write("allow:#{ip}",args[:desc],allow_time)
       end
     end
 
     desc 'Remove IPs to whitelist. Example: remove[<ips>]'
     task :remove, %i[ips] => %i[environment] do |t,args|
       args[:ips].split(',').each do |ip|
-        Rack::Attack.cache.delete("allow-#{ip}")
+        Rack::Attack.cache.delete("allow:#{ip}")
       end
     end
   end
