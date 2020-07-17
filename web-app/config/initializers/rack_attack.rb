9f2085bcc676d0b7d575aa0c910a96204c26ebc6
Felipe Jordão <felipe.japm@gmail.com>
2020-07-17 09:04:27 -0300

    Change rack attack namespace

--- web-app/config/initializers/rack_attack.rb
+++ web-app/config/initializers/rack_attack.rb
@@ -7,7 +7,8 @@ class Rack::Attack
   THROTTLE_PERIOD                         = ENV.fetch('RACK_ATTACK__THROTTLE_PERIOD_IN_MINUTES')            { 15  }
   THROTTLE_BAN_PERIOD_FOR_ABUSE_IN_DAYS   = ENV.fetch('RACK_ATTACK__THROTTLE_BAN_PERIOD_FOR_ABUSE_IN_DAYS') { 120 }
 
-  self.cache.store = ActiveSupport::Cache::RedisCacheStore.new({ url: ENV['REDIS_URL'] })
+  self.cache.prefix = "ra"
+  self.cache.store  = ActiveSupport::Cache::RedisCacheStore.new({ url: ENV['REDIS_URL'] })
 
   safelist("allowed IPs") do |req|
     self.cache.read("allow:#{req.remote_ip}")
@@ -22,9 +23,9 @@ class Rack::Attack
     limit   = THROTTLE_LIMIT * level
     period  = (THROTTLE_PERIOD * level).minutes
 
-    throttle("#{limit}req/ip/#{period}s", :limit => limit, :period => period) do |req|
+    throttle("#{limit}req/ip/#{period}sec", :limit => limit, :period => period) do |req|
 
-      cache_key = "#{Time.now.to_i/period}:#{limit}req/ip/#{period}s:#{req.remote_ip}"
+      cache_key = "#{Time.now.to_i/period}:#{limit}req/ip/#{period}sec:#{req.remote_ip}"
 
       unless req.path.start_with?('/assets')
         if req.browser.bot?
@@ -32,7 +33,7 @@ class Rack::Attack
           if inspector.trusted_claim?
             self.cache.write(
               "allow:#{req.remote_ip}", 
-              "ua=#{req.browser.name}&raw_ua=#{req.user_agent}&country=#{req.country}", 
+              "#{req.browser.name}+#{req.user_agent}+#{req.country}", 
               Time.now + ALLOW_PERIOD_FOR_TRUSTED_BOTS_IN_DAYS.days
             )
             next
@@ -42,15 +43,15 @@ class Rack::Attack
         actual_reqs = self.cache.read(cache_key).to_i
 
         if (actual_reqs >= limit)
-          acc = self.cache.read("throttled:#{req.remote_ip}:#{req.user_agent}:#{req.country}")
-          self.cache.write("throttled:#{req.remote_ip}:#{req.user_agent}:#{req.country}", acc + actual_reqs, Time.now + 1.year)
+          acc = self.cache.read("throttled:#{req.remote_ip}+#{req.user_agent}+#{req.country}")
+          self.cache.write("throttled:#{req.remote_ip}+#{req.user_agent}+#{req.country}", acc + actual_reqs, Time.now + 1.year)
 
           max_limit = THROTTLE_LEVELS * THROTTLE_LIMIT
 
           if actual_reqs >= max_limit
             self.cache.write(
               "block:#{req.remote_ip}",
-              "reqs=#{actual_reqs}&ua=#{req.browser.name}&raw_ua=#{req.user_agent}&country=#{req.country}", 
+              "#{actual_reqs}+#{req.browser.name}+#{req.user_agent}+#{req.country}", 
               (Time.now + THROTTLE_BAN_PERIOD_FOR_ABUSE_IN_DAYS.days)
             )
           end
