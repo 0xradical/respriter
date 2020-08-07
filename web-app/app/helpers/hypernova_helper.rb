b96d0fa848d6e910b24ddfec7d853b92083ccc19
Felipe Jordão <felipe.japm@gmail.com>
2020-08-07 10:27:26 -0300

    Fix call on I18n

--- web-app/app/helpers/hypernova_helper.rb
+++ web-app/app/helpers/hypernova_helper.rb
@@ -1,5 +1,5 @@
 module HypernovaHelper
   def render_component(name, data, run_context: self)
-    run_context.send(:render_vue_component, name, { locale: I18n }.merge(data.present? ? {propsData: data} : {}))
+    run_context.send(:render_vue_component, name, { locale: I18n.locale }.merge(data.present? ? {propsData: data} : {}))
   end
 end
