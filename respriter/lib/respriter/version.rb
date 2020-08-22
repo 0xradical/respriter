407d0b84ff4adb169b755825e5fb4440354dddf3
Thiago Brandão <thiagobrandam@gmail.com>
2020-08-22 11:00:09 -0300

    Remove respriter folder

--- respriter/lib/respriter/version.rb
+++
@@ -1,5 +0,0 @@
-# frozen_string_literal: true
-
-module Respriter
-  VERSION = '0.0.1'
-end
--- respriter/server/cors.js
+++
@@ -1,8 +0,0 @@
-const cors = require("cors");
-
-const corsOptions = {};
-if (process.env.SPRITE_CORS_ORIGIN) {
-  corsOptions.origin = process.env.SPRITE_CORS_ORIGIN;
-}
-
-module.exports = cors(corsOptions);
